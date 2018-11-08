77af41ad7e93da9ab6b6eca8d0885caa92e57c1e
faresginfaresgin <44483555+faresginfaresgin@users.noreply.github.com>
2018-11-08 21:10:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,15 @@ client.on('ready', () => {
  
 
 
+client.on('message', message => {
+    if(message.content.includes('discord.gg')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? 🤔   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** No Invite Links 😠 !**`)
+    }
+}
+});
+
+
+
