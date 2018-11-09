a9c0e33e774ce70f1ea7de521d60ede6414977b0
faresginfaresgin <44483555+faresginfaresgin@users.noreply.github.com>
2018-11-09 05:00:11 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(':two_hearts: بسم اللة الرحمن الرحيم :two_hearts:','https://www.twitch.tv/TEST-Broadcast%27);
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+
+
+client.login('NTA5MTE4MTA2NzcwMDc5NzQ1.DsaIuQ.Hnos_Lo2cJLeVWsNzWhqEiJm4dM');
+
+
+
+
+
+ 
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply(" الرد الي انت بدك البوت يقوله  ");
+    }
+});
+
